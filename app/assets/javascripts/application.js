b63c3504e238731cbadb484b9363a057bd1e8520
natalee36 <natalee.thomason@gmail.com>
2016-03-14 18:13:03 -0600

    remove assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require_tree ./foundation
+//= require_tree .
 
 $(function(){ $(document).foundation(); });
