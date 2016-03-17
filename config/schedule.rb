1b470b2bf6da71a96c6a9de53db9a0a15747dce4
nataleesnow <prgrmr4life@yahoo.com>
2016-03-17 13:24:55 -0600

    log path

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 #
-set :output, "../log/cron_log.log"
+set :output, "./log/cron_log.log"
 
 every 1.day do
  rake "import:transactions"
