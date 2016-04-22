c899d1d9c06f87024f9d5dffb8339858cbb13dc7
nataleesnow <prgrmr4life@yahoo.com>
2016-04-22 16:47:55 -0600

    display season

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,8 +4,8 @@
 set :output, "log/cron_log.log"
 set :environment, "development"
 
-every 1.day, :at => '5:00 pm' do
- rake "import:transactions"
-end
+#every 1.day, :at => '5:00 pm' do
+ #rake "import:transactions"
+#end
 
 # Learn more: http://github.com/javan/whenever
